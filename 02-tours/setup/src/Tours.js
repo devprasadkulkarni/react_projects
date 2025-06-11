c9fa07a6ed9e494da740dec15020cca0e4e4b084
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-06-11 19:20:04 +0530

    02-tours-01

---
+++ 02-tours/setup/src/Tours.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours }) => {
+  return (
+    <section>
+      <div className="title"></div>
+    </section>
+  );
+};
+
+export default Tours;
